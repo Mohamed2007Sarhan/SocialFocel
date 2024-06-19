cba0d81dd7e7c51db4befaadfaf6e8f75dddd5b9
Mohamed Sarhan <MohamedSarhan2990@outlook.com>
2024-06-19 14:29:48 +0300

    Add files via upload

---
+++ weating.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wait, please. ................ ["SocialFuse"] </title>
+    <link rel="icon" href="image/logo3.png">
+</head>
+
+<body>
+    <script>
+        if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+            window.location.href = "mobile.html";
+        } else {
+            window.location.href = "desktop.html";
+        }
+    </script>
+</body>
+
+</html>
+
+
+<!-- <div class="link"><p>The site was built by <span style="font-weight: bold; color: blue;"><a href="http://profile-muhammad-sarhan.eb2a.com/" style="font-weight: bold; color: blue;">Hamed Sarhan</a></span></p></div> -->
+<center>
+    <footer>
+        <em>
+            <p style="display: inline-block;">&copy; All rights reserved 2023</p>
+        </em>
+        <span style="display: inline-block;">
+            <a href="http://profile-muhammad-sarhan.eb2a.com/" style="text-decoration: none;">Muhammad Sarhan</a> -
+            <em>Website Developer</em>
+        </span>
+    </footer>
+</center>
